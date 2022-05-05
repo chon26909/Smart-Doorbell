5bf514b4fe05f1698bec743453a96efc45c3692a
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-05-05 18:05:20 +0700

    ADD MQTT BROKER

---
+++ MQTT/broker.js
@@ -0,0 +1,19 @@
+const mosca = require('mosca');
+
+const username = "chon";
+const password = "1234";
+const PORT = process.env.PORT || 1883
+const settings = { port: PORT }
+
+const broker = new mosca.Server(settings);
+
+broker.on('ready', () => {
+    console.log('Server is ready!');
+    broker.authenticate = authenticate;
+})
+
+const authenticate = (client, _username, _password, callback) => { 
+    const authorized = (_username == username  && _password.toString() == password);
+    if(authorized) client.user == username;
+    callback(null, authorized);
+}
