52dea4e6b0b9dd3dabb407be1c14be1294395ea8
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-12-21 22:45:18 +0700

    update

--- MQTT/broker.js
+++ MQTT/broker.js
@@ -1,23 +1,23 @@
-const mosca = require('mosca');
+const mosca = require("mosca");
 
 const username = "chon";
 const password = "1234";
-const PORT = process.env.PORT || 1883
-const settings = { port: PORT }
+const PORT = process.env.PORT || 1883;
+const settings = { port: PORT };
 
 const broker = new mosca.Server(settings);
 
-broker.on('ready', () => {
-    console.log('Server is ready!');
-    broker.authenticate = authenticate;
-})
+broker.on("ready", () => {
+  console.log("Server is ready!!");
+  broker.authenticate = authenticate;
+});
 
-const authenticate = (client, _username, _password, callback) => { 
-    const authorized = (_username == username  && _password.toString() == password);
-    if(authorized) client.user == username;
-    callback(null, authorized);
-}
+const authenticate = (client, _username, _password, callback) => {
+  const authorized = _username == username && _password.toString() == password;
+  if (authorized) client.user == username;
+  callback(null, authorized);
+};
 
-broker.on('published', (packet) => { 
-    console.log(packet);
-})
+broker.on("published", (packet) => {
+  console.log(packet);
+});
